d4b23013848e9229fd1a3c4551d1b6dc34364746
Jhony França <frncjhony@gmail.com>
2026-09-13 18:42:26 -0300

    Add files via upload

---
+++ Putnik/Game/View/Introducao.java
@@ -0,0 +1,42 @@
+package View;
+
+public class Introducao {
+
+    public void Contexto() {
+
+        System.out.println("\n\n");
+        System.out.println("                         17.05.2006\n");
+
+        System.out.println(
+                "Os últimos dias não estavam sendo fáceis para o nosso protagonista.\n" +
+                        "Ele havia sofrido muita pressão com os trabalhos da universidade e,\n" +
+                        "recentemente, terminado com a garota à qual havia se dedicado nos\n" +
+                        "últimos meses. Desanimado, começou a se perguntar o que poderia fazer\n" +
+                        "para ressignificar sua vida, já que sempre tivera interesse por\n" +
+                        "histórias antigas, lendas e livros do renomado H.P. Lovecraft.\n"
+        );
+
+
+        System.out.println("                         18.05.2006\n");
+
+        System.out.println(
+                "O protagonista passou a imaginar como seria viver as aventuras das\n" +
+                        "histórias que tanto gostava de ler, usando-as como uma forma de escapar\n" +
+                        "e acalmar toda aquela situação. Inclusive, começou a ler livros que,\n" +
+                        "por muito tempo, foram considerados proibidos por supostamente conterem\n" +
+                        "conhecimentos mágicos. Isso despertou dentro dele um forte desejo de,\n" +
+                        "algum dia, viver de fato as aventuras que tantas vezes havia imaginado.\n"
+        );
+
+
+        System.out.println("                         19.05.2006\n");
+
+        System.out.println(
+                "Mais um dia cansativo de aulas. O protagonista voltou para casa um\n" +
+                        "pouco mais tarde que o de costume, pois a aula havia atrasado. Ao chegar,\n" +
+                        "jogou-se no sofá da sala e prometeu a si mesmo que dormiria por apenas\n" +
+                        "cinco minutos antes de começar seus deveres de rotina...\n"
+        );
+    }
+}
+

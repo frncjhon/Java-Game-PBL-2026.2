e298fb42d8a45ac6a5b2635d76225800faafdab7
Jhony França <frncjhony@gmail.com>
2026-09-13 18:47:42 -0300

    Add files via upload

---
+++ Putnik/Game/Main.java
@@ -0,0 +1,11 @@
+import Controller.Controladordejogo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Controladordejogo controlador = new Controladordejogo();
+
+        controlador.iniciarJogo();
+    } // Inicia o jogo (Necessário)
+}

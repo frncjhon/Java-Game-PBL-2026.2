5e5b2f51416b371ec9a00bcaaad8e3a07d610bfa
Jhony França <frncjhony@gmail.com>
2026-09-20 12:09:43 -0300

    Agora Controladordejogo tem novas funções
    Limpa terminal
    Chama a criação dos demais capítulos
    ...

--- Putnik/Game/Controller/Controladordejogo.java
+++ Putnik/Game/Controller/Controladordejogo.java
@@ -33,6 +33,7 @@ public class Controladordejogo {
 
             if (menuopcao == 1) { // Se a escolha for iniciar, o contexto é apresentado
                                   // Junto com a escolha de personalidade.
+                hud.Limparterminal();
                 introducao.Contexto();
 
                 int personalidadeopcao = hud.HudPersonalidade();
@@ -49,26 +50,72 @@ public class Controladordejogo {
                 if (personalidadeopcao == 3) {
                     estado.AumentarLideranca(6);
                 }
+                hud.Limparterminal();
                 // Passa como parametros a informação que está no Estadodejogo.java
                 hud.HudStatus(
                         estado.confianca, estado.afinidadenpc1, estado.afinidadenpc2, estado.sanidade, estado.lideranca);
+                hud.AvisoEnter();
 
                 // Inicia o capitulo
+
+                // CAPÍTULO 1
                 Capitulo cena1 = ConstrutorCapitulo.criarCena("Cena 1 - O DESPERTAR", RepDialogo.cap1cen1);
                 cena1.exibirCapitulo();
-                Capitulo cena2 = ConstrutorCapitulo.criarCena("Cena 1 - O SILÊNCIO", RepDialogo.cap1cen2);
-                cena1.exibirCapitulo();
-                Capitulo cena3 = ConstrutorCapitulo.criarCena("Cena 1 - O ENCONTRO", RepDialogo.cap1cen3);
-                cena1.exibirCapitulo();
-                Capitulo cena4 = ConstrutorCapitulo.criarCena("Cena 1 - OS REGISTROS", RepDialogo.cap1cen4);
-                cena1.exibirCapitulo();
-                Capitulo cena5 = ConstrutorCapitulo.criarCena("Cena 1 - O TERCEIRO PERDIDO", RepDialogo.cap1cen5);
-                cena1.exibirCapitulo();
-                Capitulo cena6 = ConstrutorCapitulo.criarCena("Cena 1 - A PRIMEIRA NOITE", RepDialogo.cap1cen6);
-                cena1.exibirCapitulo();
-            }
+                Capitulo cena2 = ConstrutorCapitulo.criarCena("Cena 2 - O SILÊNCIO", RepDialogo.cap1cen2);
+                cena2.exibirCapitulo();
+                Capitulo cena3 = ConstrutorCapitulo.criarCena("Cena 3 - O PRIMEIRO REGISTRO", RepDialogo.cap1cen3);
+                cena3.exibirCapitulo();
+                Capitulo cena4 = ConstrutorCapitulo.criarCena("Cena 4 - TRÊS CAMINHOS", RepDialogo.cap1cen4);
+                cena4.exibirCapitulo();
+
+                hud.Limparterminal();
+                // CAPÍTULO 2
+                Capitulo cena5 = ConstrutorCapitulo.criarCena("Cena 1 - A CIDADE SEM CÉU", RepDialogo.cap2cen1);
+                cena5.exibirCapitulo();
+                Capitulo cena6 = ConstrutorCapitulo.criarCena("Cena 2 - O HOMEM DA JANELA", RepDialogo.cap2cen2);
+                cena6.exibirCapitulo();
+                Capitulo cena7 = ConstrutorCapitulo.criarCena("Cena 3 - A REGRA DO ECO", RepDialogo.cap2cen3);
+                cena7.exibirCapitulo();
+                Capitulo cena8 = ConstrutorCapitulo.criarCena("Cena 4 - O SINAL", RepDialogo.cap2cen4);
+                cena8.exibirCapitulo();
+
+                hud.Limparterminal();
+                // CAPÍTULO 3
+                Capitulo cena9 = ConstrutorCapitulo.criarCena("Cena 1 - A ESTAÇÃO", RepDialogo.cap3cen1);
+                cena9.exibirCapitulo();
+                Capitulo cena10 = ConstrutorCapitulo.criarCena("Cena 2 - MEMÓRIAS QUE NÃO PERTENCEM", RepDialogo.cap3cen2);
+                cena10.exibirCapitulo();
+                Capitulo cena11 = ConstrutorCapitulo.criarCena("Cena 3 - O NOME ESCRITO", RepDialogo.cap3cen3);
+                cena11.exibirCapitulo();
+                Capitulo cena12 = ConstrutorCapitulo.criarCena("Cena 4 - A FALSA SAÍDA", RepDialogo.cap3cen4);
+                cena12.exibirCapitulo();
+
+                hud.Limparterminal();
+                // CAPÍTULO 4
+                Capitulo cena13 = ConstrutorCapitulo.criarCena("Cena 1 - A CASA DE BAIXO", RepDialogo.cap4cen1);
+                cena13.exibirCapitulo();
+                Capitulo cena14 = ConstrutorCapitulo.criarCena("Cena 2 - O PREÇO", RepDialogo.cap4cen2);
+                cena14.exibirCapitulo();
+                Capitulo cena15 = ConstrutorCapitulo.criarCena("Cena 3 - A VERDADE DE DEYV", RepDialogo.cap4cen3);
+                cena15.exibirCapitulo();
+                Capitulo cena16 = ConstrutorCapitulo.criarCena("Cena 4 - O RELÓGIO", RepDialogo.cap4cen4);
+                cena16.exibirCapitulo();
+
+
+                hud.Limparterminal();
+                // CAPÍTULO 5
+                Capitulo cena17 = ConstrutorCapitulo.criarCena("Cena 1 - A PRISÃO UNIVERSAL", RepDialogo.cap5cen1);
+                cena17.exibirCapitulo();
+                Capitulo cena18 = ConstrutorCapitulo.criarCena("Cena 2 - O CENTRO", RepDialogo.cap5cen2);
+                cena18.exibirCapitulo();
+                Capitulo cena19 = ConstrutorCapitulo.criarCena("Cena 3 - A ESCOLHA", RepDialogo.cap5cen3);
+                cena19.exibirCapitulo();
+                Capitulo cena20 = ConstrutorCapitulo.criarCena("Cena 4 - O ÚLTIMO PASSAGEIRO", RepDialogo.cap5cen4);
+                cena20.exibirCapitulo();}
+
 
             // "Fecha" o jogo
+            hud.Limparterminal();
             if (menuopcao == 2) {
                 System.out.println(" Fechando o Jogo...");
                 System.out.println("(Obrigado por jogar)");

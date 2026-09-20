6a2e9735cc138054d3e2c4750d52267ff0f553ae
Jhony França <frncjhony@gmail.com>
2026-09-20 12:12:45 -0300

    Mudanças em dialogo
    Adicionei um método para imprimir apenas texto, sem a necessidade de definir o personagem.
    Removi a mensagem de ENTER

--- Putnik/Game/Model/Dialogo.java
+++ Putnik/Game/Model/Dialogo.java
@@ -6,12 +6,20 @@ public class Dialogo {
     private String personagem;
     private String texto;
 
+
+    public Dialogo(String texto) {
+        this.texto = texto;
+    }
     public Dialogo(String personagem, String texto) {
         this.personagem = personagem;
         this.texto = texto;
     }
 
-    public void exibir(){
-        System.out.println(personagem + ": " +  texto);
+    public void exibir() {
+        if (personagem != null) {
+            System.out.println(personagem + ": " + texto);
+        } else {
+            System.out.println(texto);
+        }
     }
 }

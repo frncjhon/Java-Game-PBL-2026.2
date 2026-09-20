d982918efc4f32c98ff77c3806495f06735c61db
Cleuber-404 <cleuber.facul@gmail.com>
2026-09-20 16:41:56 -0300

    Create Opcao class for game options

---
+++ Putnik/Game/Model/Opcao.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.function.Consumer;
+
+public class Opcao {
+    private String texto;
+    private Consumer<Estadodejogo> efeito;
+
+    public Opcao(String texto, Consumer<Estadodejogo> efeito){
+        this.texto = texto;
+        this.efeito = efeito;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void aplicarEfeito(Estadodejogo estado){
+        if (efeito != null){
+            efeito.accept(estado);
+        }
+    }
+}

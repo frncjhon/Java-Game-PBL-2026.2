9129ac8b5c7d2e4e9f3a246631d28089e69ffa9b
Jhony França <frncjhony@gmail.com>
2026-09-20 14:28:23 -0300

    Controladordejogo organizado

--- Putnik/Game/Controller/Controladordejogo.java
+++ Putnik/Game/Controller/Controladordejogo.java
@@ -13,7 +13,6 @@ public class Controladordejogo {
     private Introducao introducao;
     private Hud hud;
 
-
 // Criação das instâncias
     public Controladordejogo() {
         estado = new Estadodejogo();
@@ -23,7 +22,7 @@ public class Controladordejogo {
     }
 
 
-    // Inicia o Menu, e fica responsável por fazer todo o inicio do jogo.
+    // Inicio, resp. controlar o andamento do jogo.
     public void iniciarJogo() {
         boolean executando = true;
 
@@ -33,30 +32,36 @@ public class Controladordejogo {
 
             if (menuopcao == 1) { // Se a escolha for iniciar, o contexto é apresentado
                                   // Junto com a escolha de personalidade.
+
                 hud.Limparterminal();
                 introducao.Contexto();
 
-                int personalidadeopcao = hud.HudPersonalidade();
 
+                int personalidadeopcao = hud.HudPersonalidade();
                 if (personalidadeopcao == 1) {
                     estado.AumentarAfinidadeNPC1(6);
                     estado.AumentarAfinidadeNPC2(6);
                 }
-
                 if (personalidadeopcao == 2) {
                     estado.AumentarConfianca(3);
                 }
-
                 if (personalidadeopcao == 3) {
                     estado.AumentarLideranca(6);
                 }
+
+
                 hud.Limparterminal();
                 // Passa como parametros a informação que está no Estadodejogo.java
                 hud.HudStatus(
-                        estado.confianca, estado.afinidadenpc1, estado.afinidadenpc2, estado.sanidade, estado.lideranca);
+                        estado.confianca, estado.afinidadenpc1,
+                        estado.afinidadenpc2,
+                        estado.sanidade,
+                        estado.lideranca);
                 hud.AvisoEnter();
 
-                // Inicia o capitulo
+
+
+                // Inicia Narrativa
 
                 // CAPÍTULO 1
                 Capitulo cena1 = ConstrutorCapitulo.criarCena("Cena 1 - O DESPERTAR", RepDialogo.cap1cen1);
@@ -67,9 +72,16 @@ public class Controladordejogo {
                 cena3.exibirCapitulo();
                 Capitulo cena4 = ConstrutorCapitulo.criarCena("Cena 4 - TRÊS CAMINHOS", RepDialogo.cap1cen4);
                 cena4.exibirCapitulo();
-
                 hud.Limparterminal();
+
                 // CAPÍTULO 2
+                hud.HudStatus(
+                        estado.confianca,
+                        estado.afinidadenpc1,
+                        estado.afinidadenpc2,
+                        estado.sanidade,
+                        estado.lideranca);
+
                 Capitulo cena5 = ConstrutorCapitulo.criarCena("Cena 1 - A CIDADE SEM CÉU", RepDialogo.cap2cen1);
                 cena5.exibirCapitulo();
                 Capitulo cena6 = ConstrutorCapitulo.criarCena("Cena 2 - O HOMEM DA JANELA", RepDialogo.cap2cen2);
@@ -78,9 +90,16 @@ public class Controladordejogo {
                 cena7.exibirCapitulo();
                 Capitulo cena8 = ConstrutorCapitulo.criarCena("Cena 4 - O SINAL", RepDialogo.cap2cen4);
                 cena8.exibirCapitulo();
-
                 hud.Limparterminal();
+
                 // CAPÍTULO 3
+                hud.HudStatus(
+                        estado.confianca,
+                        estado.afinidadenpc1,
+                        estado.afinidadenpc2,
+                        estado.sanidade,
+                        estado.lideranca);
+
                 Capitulo cena9 = ConstrutorCapitulo.criarCena("Cena 1 - A ESTAÇÃO", RepDialogo.cap3cen1);
                 cena9.exibirCapitulo();
                 Capitulo cena10 = ConstrutorCapitulo.criarCena("Cena 2 - MEMÓRIAS QUE NÃO PERTENCEM", RepDialogo.cap3cen2);
@@ -89,9 +108,16 @@ public class Controladordejogo {
                 cena11.exibirCapitulo();
                 Capitulo cena12 = ConstrutorCapitulo.criarCena("Cena 4 - A FALSA SAÍDA", RepDialogo.cap3cen4);
                 cena12.exibirCapitulo();
-
                 hud.Limparterminal();
+
                 // CAPÍTULO 4
+                hud.HudStatus(
+                        estado.confianca,
+                        estado.afinidadenpc1,
+                        estado.afinidadenpc2,
+                        estado.sanidade,
+                        estado.lideranca);
+
                 Capitulo cena13 = ConstrutorCapitulo.criarCena("Cena 1 - A CASA DE BAIXO", RepDialogo.cap4cen1);
                 cena13.exibirCapitulo();
                 Capitulo cena14 = ConstrutorCapitulo.criarCena("Cena 2 - O PREÇO", RepDialogo.cap4cen2);
@@ -100,10 +126,16 @@ public class Controladordejogo {
                 cena15.exibirCapitulo();
                 Capitulo cena16 = ConstrutorCapitulo.criarCena("Cena 4 - O RELÓGIO", RepDialogo.cap4cen4);
                 cena16.exibirCapitulo();
-
-
                 hud.Limparterminal();
+
                 // CAPÍTULO 5
+                hud.HudStatus(
+                        estado.confianca,
+                        estado.afinidadenpc1,
+                        estado.afinidadenpc2,
+                        estado.sanidade,
+                        estado.lideranca);
+
                 Capitulo cena17 = ConstrutorCapitulo.criarCena("Cena 1 - A PRISÃO UNIVERSAL", RepDialogo.cap5cen1);
                 cena17.exibirCapitulo();
                 Capitulo cena18 = ConstrutorCapitulo.criarCena("Cena 2 - O CENTRO", RepDialogo.cap5cen2);
@@ -112,6 +144,30 @@ public class Controladordejogo {
                 cena19.exibirCapitulo();
                 Capitulo cena20 = ConstrutorCapitulo.criarCena("Cena 4 - O ÚLTIMO PASSAGEIRO", RepDialogo.cap5cen4);
                 cena20.exibirCapitulo();}
+
+
+
+            // Condições para os finais de jogo.
+
+            if (estado.afinidadenpc1 > estado.afinidadenpc2){
+                Capitulo Final = ConstrutorCapitulo.criarCena(
+                        "Final", RepDialogo.final1);
+            }
+            if (estado.afinidadenpc2 > estado.afinidadenpc1) {
+                Capitulo Final = ConstrutorCapitulo.criarCena(
+                        "Final", RepDialogo.final2);
+            }
+
+
+            // Ainda não definido como os status serão incrementados então deixei o molde pronto.]
+            // Mas a ideia é verificar se os status estão baixos
+            if (estado.lideranca == 0 && estado.confianca == 0 && estado.sanidade == 0) {
+                Capitulo Final = ConstrutorCapitulo.criarCena(
+                        "Final", RepDialogo.final3);
+            }
+            // não foi implementado o final secreto.
+
+
 
 
             // "Fecha" o jogo

737160a68f2247f24ea3e65c750ef5bf4bd984a9
Jhony França <frncjhony@gmail.com>
2026-09-21 13:03:19 -0300

    Repost da Entrega Fase 01

--- Putnik/Game/Main.java
+++ Putnik/Game/Main.java
@@ -1,11 +1,9 @@
 import Controller.Controladordejogo;
-
 public class Main {
-
+    
     public static void main(String[] args) {
-
         Controladordejogo controlador = new Controladordejogo();
-
         controlador.iniciarJogo();
     } // Inicia o jogo (Necessário)
+    // Putnik V1.0 - Jhony Jrança, Cleuber Coelho
 }

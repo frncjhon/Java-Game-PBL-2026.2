bb01ef4fee5401a5f968333fed0e64664564a3e1
Jhony França <frncjhony@gmail.com>
2026-09-21 03:45:57 -0300

    Para Entrega!

--- Putnik/Game/View/Introducao.java
+++ Putnik/Game/View/Introducao.java
@@ -4,39 +4,26 @@ public class Introducao {
 
     public void Contexto() {
 
-        System.out.println("\n\n");
-        System.out.println("                         17.05.2006\n");
-
         System.out.println(
                 "Os últimos dias não estavam sendo fáceis para o nosso protagonista.\n" +
-                        "Ele havia sofrido muita pressão com os trabalhos da universidade e,\n" +
-                        "recentemente, terminado com a garota à qual havia se dedicado nos\n" +
-                        "últimos meses. Desanimado, começou a se perguntar o que poderia fazer\n" +
-                        "para ressignificar sua vida, já que sempre tivera interesse por\n" +
-                        "histórias antigas, lendas e livros do renomado H.P. Lovecraft.\n"
+                        "Ele havia enfrentado muita pressão com os trabalhos da universidade e,\n" +
+                        "recentemente, terminado com a garota à qual havia se dedicado nos últimos meses.\n" +
+                        "Desanimado, começou a se perguntar o que poderia fazer para ressignificar sua vida.\n" +
+                        "Sempre tivera interesse por histórias antigas, lendas e pelos livros do renomado\n" +
+                        "H.P. Lovecraft, e passou a imaginar como seria viver as aventuras que tanto\n" +
+                        "gostava de ler, usando-as como uma forma de escapar e aliviar aquela situação.\n\n" +
+
+                        "Movido por essa curiosidade, começou a ler livros que, durante muito tempo,\n" +
+                        "foram considerados proibidos por supostamente conterem conhecimentos mágicos.\n" +
+                        "A leitura despertou nele um forte desejo de, algum dia, viver de fato as aventuras\n" +
+                        "que tantas vezes havia imaginado.\n\n" +
+
+                        "Após mais um dia cansativo de aulas, o protagonista voltou para casa um pouco\n" +
+                        "mais tarde que o habitual. Ao chegar, jogou-se no sofá da sala e prometeu a si\n" +
+                        "mesmo que dormiria por apenas cinco minutos antes de começar seus deveres de rotina...\n"
         );
 
-
-        System.out.println("                         18.05.2006\n");
-
-        System.out.println(
-                "O protagonista passou a imaginar como seria viver as aventuras das\n" +
-                        "histórias que tanto gostava de ler, usando-as como uma forma de escapar\n" +
-                        "e acalmar toda aquela situação. Inclusive, começou a ler livros que,\n" +
-                        "por muito tempo, foram considerados proibidos por supostamente conterem\n" +
-                        "conhecimentos mágicos. Isso despertou dentro dele um forte desejo de,\n" +
-                        "algum dia, viver de fato as aventuras que tantas vezes havia imaginado.\n"
-        );
-
-
-        System.out.println("                         19.05.2006\n");
-
-        System.out.println(
-                "Mais um dia cansativo de aulas. O protagonista voltou para casa um\n" +
-                        "pouco mais tarde que o de costume, pois a aula havia atrasado. Ao chegar,\n" +
-                        "jogou-se no sofá da sala e prometeu a si mesmo que dormiria por apenas\n" +
-                        "cinco minutos antes de começar seus deveres de rotina...\n"
-        );
     }
 }
+
 

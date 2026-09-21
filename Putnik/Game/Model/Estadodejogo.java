8f8122bea1a3d40842e6b0b12c99e076a80a6217
Jhony França <frncjhony@gmail.com>
2026-09-21 12:57:01 -0300

    Repost da Entrega Fase 01

--- Putnik/Game/Model/Estadodejogo.java
+++ Putnik/Game/Model/Estadodejogo.java
@@ -1,12 +1,10 @@
-package Model;
-
+package Model; // Informa a qual grupo o arquivo pertence.
 public class Estadodejogo {
 
     // Status do protagonista
     public double sanidade;
     public double lideranca;
     public double confianca;
-
     // Afinidade com os NPCs
     public double afinidadenpc1;
     public double afinidadenpc2;
@@ -17,37 +15,33 @@ public class Estadodejogo {
         sanidade = 10;
         lideranca = 0;
         confianca = 0;
-
         afinidadenpc1 = 0;
         afinidadenpc2 = 0;
     }
 
-
     // Métodos para Status do Protagonista
-
-    // Sanidade
-    public void AumentarSanidade(double valor) {
+    public void AumentarSanidade(double valor) {    // Sanidade
         sanidade = sanidade + valor;
     }
-
-    // Liderança
-    public void AumentarLideranca(double valor) {
+    public void AumentarLideranca(double valor) {   // Liderança
         lideranca = lideranca + valor;
     }
-    // Confiança
-    public void AumentarConfianca(double valor) {
+    public void AumentarConfianca(double valor) {   // Confiança
         confianca = confianca + valor;
     }
 
-// Métodos para alterar afinidade com NPCs
+    // Métodos para alterar afinidade com NPCs
+
+    // Apesar do metodo se chamar AUMENTAR ele pode diminuir. Não foi pensado nessa possibilidade durante
+    // o desenvolvimento incial, mas no entanto decidimos manter este nome.
+
     // NPC1
     public void AumentarAfinidadeNPC1(double valor) {
         afinidadenpc1 = afinidadenpc1 + valor;
     }
-
     // NPC2
     public void AumentarAfinidadeNPC2(double valor) {
         afinidadenpc2 = afinidadenpc2 + valor;
     }
-
 }
+// Fim

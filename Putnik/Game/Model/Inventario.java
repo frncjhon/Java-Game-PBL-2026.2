25319b445fd1b6b484ea02eab26db3f2c2d59f64
Jhony França <frncjhony@gmail.com>
2026-09-21 12:57:33 -0300

    Repost da Entrega Fase 01

--- Putnik/Game/Model/Inventario.java
+++ Putnik/Game/Model/Inventario.java
@@ -1,4 +1,4 @@
-package Model;
+package Model; // Informa a qual grupo o arquivo pertence.
 
 public class Inventario {
 
@@ -6,17 +6,14 @@ public class Inventario {
     public int ChaveMikhail = 0;
     public int SimboloElena = 0;
 
-
     // Adiciona a Chave de Mikhail
     public void AdicionarChaveMikhail() {
         ChaveMikhail = 1;
     }
 
-
     // Adiciona o Símbolo de Elena
     public void AdicionarSimboloElena() {
         SimboloElena = 1;
     }
-
 }
-
+// Fim

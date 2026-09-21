d7d62b1fb3a4579f7fdce22d09ccc28e813e5230
Jhony França <frncjhony@gmail.com>
2026-09-21 13:00:47 -0300

    Repost da Entrega Fase 01

--- Putnik/Game/View/ConstrutorCapitulo.java
+++ Putnik/Game/View/ConstrutorCapitulo.java
@@ -1,9 +1,10 @@
-package View;
+package View; // Informa a qual grupo o arquivo pertence.
 import Model.Dialogo;
-import Model.RepDialogo;
 
+// Cria e retorna um capítulo com TITULO e DIALOGO informado.
 public class ConstrutorCapitulo {
     public static Capitulo criarCena(String titulo, Dialogo[] cena){
         return new Capitulo(titulo,cena);
     }
 }
+// Fim

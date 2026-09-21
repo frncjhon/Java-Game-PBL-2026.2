71b7e6cca511d0ec9d35f238507ec437a7340328
Jhony França <frncjhony@gmail.com>
2026-09-21 12:55:54 -0300

    Repost da Entrega Fase 01

--- Putnik/Game/Model/Dialogo.java
+++ Putnik/Game/Model/Dialogo.java
@@ -1,18 +1,19 @@
-package Model;
+package Model; // Informa a qual grupo o arquivo pertence.
 
 public class Dialogo {
     private String personagem;
     private String texto;
 
-
+    // Inicializa um diálogo apenas com o texto.
     public Dialogo(String texto) {
         this.texto = texto;
     }
+    // Inicializa um diálogo definindo o personagem e o texto da fala.
     public Dialogo(String personagem, String texto) {
         this.personagem = personagem;
         this.texto = texto;
     }
-
+    // Exibe
     public void exibir() {
         if (personagem != null) {
             System.out.println(personagem + ": " + texto);
@@ -21,3 +22,4 @@ public class Dialogo {
         }
     }
 }
+// Fim
